30800f2a5447490d56fc88fd9a9777164897c0be
fernando2424 <marvinmarvin19911991@gmail.com>
2017-05-30 16:51:54 -0400

    commit numero 11

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+      Meteor.methods({
+      	"checkConnection" : function (id){
+      	 var result	= CONNECT.find({idUs:id,stade:true}).fetch();
+      	 if(result.lenght>0){
+      	 	return {value:true,id:result[0]._id};
+      	 }
+      	 return {value:true};
+      	},
+      	"createConnection": function(idus){
+                  console.log(idus);
+                  var id = CONNECT.insert({idUs:idus,connectionDate:new Date(),disconnectionDate:new Date(),stade:true});
+                  return id;
+            },
+            "disconnection": function(id){
+                  CONNECT.update(id,{$set:{stade:false,disconnectionDate:new Date()}});
+                  return true;
+            }
+
+      });
 });
